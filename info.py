cf7514e227232ce205088d68f3110f219d047233
freeboss29 <140298202+freeboss29@users.noreply.github.com>
2026-02-02 13:58:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -173,7 +173,7 @@ if "DYNO" in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "https://filter-bot-alyw.onrender.com/")
 
 # Commands
 admin_cmds = [
