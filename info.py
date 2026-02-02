dc9b53d9e824305038ac75eefe442ea839c3ec30
freeboss29 <140298202+freeboss29@users.noreply.github.com>
2026-02-02 13:55:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ def is_enabled(value, default):
 SESSION = environ.get("SESSION", "Media_search")
 API_ID = int(environ.get("API_ID", "26410878"))
 API_HASH = environ.get("API_HASH", "3fd13024f9e777877422887da6e1ac28")
-BOT_TOKEN = environ.get("BOT_TOKEN", "782873952 ")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 PORT = environ.get("PORT", "8082")
 
 # Owners
